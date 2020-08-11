a829d1da596de3c500e8131eca167f08f08b40bc
ChenYibo <charbo233@foxmail.com>
2020-08-11 19:45:11 +0800

    版本号刷新规则更新

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const timestamp = new Date().getTime();
 module.exports = {
   // 关闭map文件
   productionSourceMap: false,
@@ -21,5 +21,11 @@ module.exports = {
   chainWebpack: (config) => {
     config.resolve.alias.set('@', path.resolve(__dirname, 'src')).set('@mock', path.resolve(__dirname, 'mock'));
     config.performance.set('maxAssetSize', 1024 * 1024).set('maxEntrypointSize', 1024 * 1024);
+  },
+  configureWebpack: { // webpack 配置
+    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+      filename: `[name].[chunkhash].${timestamp}.js`,
+      chunkFilename: `[name].[chunkhash].${timestamp}.js`
+    },
   },
 };
