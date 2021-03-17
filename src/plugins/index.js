05cfb557933118fd6a04d3b927935704afbee96b
ChenYibo <charbo233@foxmail.com>
2021-03-17 15:31:18 +0800

    去除冗余内容

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
-import echarts from 'echarts';
-import 'echarts-amap';
 import { DatePicker, Row, Col, Progress, Message, Carousel } from 'ant-design-vue';
-
-Vue.prototype.$echarts = echarts;
 
 Vue.use(DatePicker)
   .use(Row)
